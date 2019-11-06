abd365beb6fb155f371485c0f7835f62ca743931
Tom Ekander <ekander.tom@gmail.com>
2019-11-06 13:01:59 +0100

    chore: try a different order for postcss

--- postcss.config.js
+++ postcss.config.js
@@ -5,8 +5,8 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
 
 module.exports = {
   plugins: [
-    require('tailwindcss'),
     require('autoprefixer'),
+    require('tailwindcss'),
     ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
   ],
 }
