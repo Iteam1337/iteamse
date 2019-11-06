73fdc206320a33a3bf30a63c5980617ee21389d1
Tom Ekander <ekander.tom@gmail.com>
2019-11-06 15:33:31 +0100

    chore: does it work without purge-css

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,13 @@
-const purgecss = require('@fullhuman/postcss-purgecss')({
-  content: ['./lib/**/*.js'],
-  css: ['./src/index.css'],
-  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
-})
+// const purgecss = require('@fullhuman/postcss-purgecss')({
+//   content: ['./lib/**/*.js'],
+//   css: ['./src/index.css'],
+//   defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+// })
 
 module.exports = {
   plugins: [
     require('tailwindcss'),
     require('autoprefixer'),
-    ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
+    ...(process.env.NODE_ENV === 'production' ? [] : []),
   ],
 }
