14e8f8b0ecac270c7ee57af1a6c126ac5f485bef
Tom Ekander <ekander.tom@gmail.com>
2019-11-06 15:31:33 +0100

    chore: will it need the path after build

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,6 @@
 const purgecss = require('@fullhuman/postcss-purgecss')({
-  content: ['./lib/**/*.js', 'index.css'],
+  content: ['./lib/**/*.js'],
+  css: ['./src/index.css'],
   defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
 })
 
